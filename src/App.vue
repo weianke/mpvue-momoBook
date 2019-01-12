d6bc0c77766e1c2392b8819caabb318fa07ee491
anke <245783065@qq.com>
2019-01-12 16:18:31 +0800

    edit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import {get} from 'utils/request'
 export default {
   async created () {
-    const res = await get('/webapp/demo')
+    const res = await get('/weapp/demo')
     console.log(res)
   }
 }
--- src/config.js
+++ src/config.js
@@ -3,7 +3,9 @@
 const host = 'http://localhost:5757'
 
 const config = {
-  host
+  host,
+  loginUrl: `${host}/weapp/login`,
+  userUrl: `${host}/weapp/user`
 }
 
 export default config
